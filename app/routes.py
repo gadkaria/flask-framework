9f9fd82adf90c5bed30578c9e81cbee23518022a
gadkari <gadkari@umich.edu>
2019-06-06 14:36:16 -0500

    Create Basic Forms

---
+++ app/routes.py
@@ -0,0 +1,27 @@
+from flask import render_template, flash, redirect
+from app import app
+from app.forms import LoginForms
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = { 'username':'Ameya'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title="Home", user=user, posts=posts)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    forms = LoginForms()
+    if forms.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(forms.username.data, forms.remember_me.data))
+        return redirect("/index")
+    return render_template('login.html', title="Sign In", form=forms) 
